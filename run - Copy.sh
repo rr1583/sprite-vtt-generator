99f8d977ff0533a0ee7e714df4b77dda76a29b2d
rr1583 <rr1583@gmail.com>
2025-06-16 11:47:09 -0400

    Revert nginx container ID check

--- run - Copy.sh
+++ run - Copy.sh
@@ -309,9 +309,8 @@ echo ""
 # now spin up the nginx to show the results / test video and images #
 #####################################################################
 
-# Get nginx container ID if running; ignore errors so set -e doesn't exit
-NGINX_ID="$(docker-compose ps -q mt-nginx 2>/dev/null || true)"
-if [ -z "$NGINX_ID" ] || ! docker ps -q --no-trunc --filter "id=$NGINX_ID" | grep -q .; then
+# See if nginx is already running and start it if not
+if [ -z `docker-compose ps -q mt-nginx` ] || [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q mt-nginx)` ]; then
   echo ""
   echo "Nginx is not running so starting up..."
   docker-compose up -d mt-nginx
